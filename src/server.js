bcde49366081f7208d868b851787cc0191646d70
Lukasz Czapraga <lczapraga@gmail.com>
2022-11-01 22:31:21 +0100

    0.0.01 - Created first GET - for username

--- src/server.js
+++ src/server.js
@@ -1,1 +1,15 @@
 console.log("REST SERVER APPLICATION");
+
+const express = require('express');
+const app = express();
+const port = 8081;
+
+app.get('/home', (req, res) => res.send('Nasz pierwszy serwer w Node.JS'));
+app.listen(port, () => console.log(`Aplikacja działa na porcie ${port}`));
+
+
+app.get('/salut/:userName', function (req, res) {
+    let userName = req.params.userName;
+    console.log(`Request userName=${userName}`);
+    res.status(200).send(`Hello ${userName}!`);
+});
